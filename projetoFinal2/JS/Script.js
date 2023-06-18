86f08788aac4b2d5d5ece479bf065bd485d5d9ec
BiaOliveira12 <aguiarbeatriz12ba@gmail.com>
2023-06-18 18:40:52 -0300

    Atualizacao de commit

---
+++ projetoFinal2/JS/Script.js
@@ -0,0 +1,15 @@
+let count = 1;
+var contagem = document.getElementById("slide1").checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 5000)
+
+function nextImage(){
+    count++;
+    if(count>5){
+        count = 1;
+    }
+
+document.getElementById("slide"+count).checked = true;
+}
